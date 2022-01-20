70ba31080be9176cd0b5ca315ff446abd33a01a1
Rohan Kokane <rohankokane19@gmail.com>
2022-01-20 14:02:07 +0530

    db auth andn cors fixed

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-const path = require('path')
-
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
@@ -7,23 +5,22 @@ const mongoose = require('mongoose')
 const postsRoutes = require('./routes/posts-routes')
 const usersRoutes = require('./routes/users-routes')
 const HttpError = require('./models/http-error')
-
+const cors = require('cors')
 const app = express()
 
+app.options('*', cors())
+app.use(cors())
 app.use(bodyParser.json())
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*')
+//   res.setHeader(
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+//   )
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE')
 
-// app.use('/uploads/images', express.static(path.join('uploads', 'images')));
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  )
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE')
-
-  next()
-})
+//   next()
+// })
 
 app.use('/api/users', usersRoutes)
 app.use('/api/posts', postsRoutes)
@@ -34,23 +31,28 @@ app.use((req, res, next) => {
 })
 
 app.use((error, req, res, next) => {
-  // if (req.file) {
-  //   fs.unlink(req.file.path, err => {
-  //     console.log(err);
-  //   });
+  // if (res.headerSent) {
+  //   return next(error)
   // }
-  if (res.headerSent) {
-    return next(error)
-  }
   res.status(error.code || 500)
   res.json({ message: error.message || 'An unknown error occurred!' })
 })
+// mongoose.connect(
+//   `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wtuap.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+// )
 mongoose
   .connect(
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wtuap.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+    'mongodb+srv://cluster0.wtuap.mongodb.net/bookworms?retryWrites=true&w=majority',
+    {
+      user: process.env.DB_USER,
+      pass: process.env.DB_PASSWORD,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
   )
   .then(() => {
-    app.listen(process.env.PORT || 5000)
+    app.listen(process.env.PORT)
+    console.log('db connected')
   })
   .catch((err) => {
     console.log(err)
